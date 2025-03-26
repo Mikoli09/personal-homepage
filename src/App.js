4635929098361034e0f2d5ac7ecfed47c1ed5eaa
Marcin Osojca <osojca.marcin1@gmail.com>
2025-03-26 12:22:57 +0100

    theme switching adjusted at page reload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import { Header } from './features/Homepage/MainHeader';
 import { MainContainer } from './common/MainContainer/styled';
-import { Section } from './common/SkillsSection';
-import { Skills } from './features/Homepage/SkillsSection';
+import { Section } from './features/Homepage/SkillsSection';
+import { Skills } from './common/Skills';
 import { skillsList } from './assets/skillsList';
 import { toLearnList } from './assets/toLearnList';
 import { Portfolio } from './features/Homepage/Portfolio';
@@ -12,13 +12,15 @@ import { Footer } from './features/Homepage/Footer';
 import { ThemeLight, ThemeDark } from './themeProvider';
 import { selectThemeDark } from './features/Homepage/homepageSlice';
 import { useSelector } from 'react-redux';
+import { useRefreshDisable } from './useRefreshDisable';
 
 function App() {
   const darkThemeSelected = useSelector(selectThemeDark);
+  const refreshDisabled = useRefreshDisable();
 
   return (
-    <ThemeProvider theme={darkThemeSelected? ThemeDark : ThemeLight}>
-      <GlobalStyle />
+    <ThemeProvider theme={darkThemeSelected ? ThemeDark : ThemeLight}>
+      <GlobalStyle refreshDisabled={refreshDisabled} />
       <MainContainer>
         <Header />
         <Section
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -13,6 +13,6 @@ export const GlobalStyle = createGlobalStyle`
         font-family: "Inter", sans-serif;
         background-color: ${({theme})=> theme.colors.body.background};
         margin:0;
-        transition: 0.3s;
+        transition: ${({refreshDisabled}) => refreshDisabled ? "none" : "0.3s"};
     }
 `;
